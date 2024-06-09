90a81e76b79f0e00437d41a5f5944a0c7058ee6e
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-06-09 14:38:29 +0700

    Navbar Hover

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import Navbar from '@/scenes/navbar';
+import { useState } from 'react';
 
 function App() {
-
+  const [selectedPage, setSelectedPage] = useState<string>('home')
   return (
       <div className='app bg-gray-20'>
-        <Navbar />
+        <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
       </div>
   )
 }
-
 export default App
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+// import { useState } from "react";
+// import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from '@/assets/Logo.png'; 
+import Link from "./Link";
 
-type Props = {}
+type Props = {
+    selectedPage: string;
+    setSelectedPage: (value: string) => void;}
 
-const Navbar = (props: Props) => {
+const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
     const flexBetween = 'flex items-center justify-between';
   return (
     <nav>
@@ -18,10 +21,10 @@ const Navbar = (props: Props) => {
                     <div className={`${flexBetween} w-full`}>
                         {/* INNER LEFT SIDE */}
                         <div className={`${flexBetween} gap-8 text-sm`}>
-                            <p>Home</p>
-                            <p>Benefits</p>
-                            <p>Our Classes</p>
-                            <p>Contact Us</p>
+                            <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+                            <Link page="Benefits" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+                            <Link page="Our Classes" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+                            <Link page="Contact Us" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
                         </div>
                         {/* INNER RIGHT SIDE */}
                         <div className={`${flexBetween} gap-8`}>
