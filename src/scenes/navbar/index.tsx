35488c80f1cb3ef6f7ff1bf7abf90fbe804e4739
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-06-10 16:04:10 +0700

    Building for Below Medium Screens

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,8 +1,10 @@
 // import { useState } from "react";
-// import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from '@/assets/Logo.png'; 
 import Link from "./Link";
 import { SelectedPage } from '@/shared/types';
+import useMediaQuery from '@/hooks/useMediaQuery';
+import { useState } from "react";
 
 type Props = {
     selectedPage: SelectedPage;
@@ -10,6 +12,8 @@ type Props = {
 
 const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
     const flexBetween = 'flex items-center justify-between';
+    const isAboveMediumScreens = useMediaQuery('(min-width: 1060px)');
+    const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
   return (
     <nav>
         <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
@@ -19,7 +23,7 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
                     <img alt="logo"  src={Logo} />
 
                     {/* RIGHT SIDE */}
-                    <div className={`${flexBetween} w-full`}>
+                    {isAboveMediumScreens ? (<div className={`${flexBetween} w-full`}>
                         {/* INNER LEFT SIDE */}
                         <div className={`${flexBetween} gap-8 text-sm`}>
                             <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
@@ -32,7 +36,13 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
                             <p>Sign In</p>
                             <button>Become a Member</button>
                         </div>
-                    </div>
+                    </div>) : (
+                        <button className='rounded-full bg-secondary-500 p-2'
+                        onClick={() => setIsMenuToggled(!isMenuToggled)}
+                        >
+                            <Bars3Icon className="h-6 w-6 text-white" />
+                        </button>
+                    )}
                 </div>
             </div>
         </div>
