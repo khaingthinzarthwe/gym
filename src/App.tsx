db64e6ad076009f0729ecfec3341780e48b42b89
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-06-11 22:38:00 +0700

    Base Layout Home Page Main Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from '@/scenes/navbar';
 import { useEffect, useState } from 'react';
 import { SelectedPage } from '@/shared/types';
+import Home from '@/scenes/home';
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
@@ -21,6 +22,7 @@ function App() {
   return (
       <div className='app bg-gray-20'>
         <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+        <Home setSelectedPage={setSelectedPage} />
       </div>
   )
 }
---
+++ src/scenes/home/index.tsx
@@ -0,0 +1,55 @@
+import { SelectedPage } from '@/shared/types';
+import useMediaQuery from '@/hooks/useMediaQuery';
+import ActionButton from '@/shared/ActionButton';
+import HomePageText from '@/assets/HomePageText.png';
+import HomePageGraphic from '@/assets/HomePageGraphic.png';
+import SponsorRedBull from '@/assets/SponsorRedBull.png';
+import SponsorForbes from '@/assets/SponsorForbes.png';
+import SponsorFoutune from '@/assets/SponsorFoutune.png';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+
+type Props = {
+    setSelectedPage: (value: SelectedPage) => void;
+}
+
+const Home = ({ setSelectedPage }: Props) => {
+    const isAboveMediumScreens = useMediaQuery('(min-width: 1060px)');
+  return (
+    <section id='home' className='gap-16 bg-gray-20 py-10 md:h-full md:pb-0'>
+        {/* IMAGE AND MAIN HEADER */}
+        <div>
+            {/* MAIN HEADER */}
+            <div>
+                {/* HEADING */}
+                <div>
+                    <div>
+                        <div>
+                            <img alt='home-page-text' src={HomePageText} />
+                        </div>
+                    </div>
+                    <p>
+                        Unrivaled Gym. Unparalled Training Fitness Classes. World Class Studios to get
+                        the Body Shapes That you Dream of...Get Your Dream Body Now.
+                    </p>
+                </div>
+                {/* ACTIONS */}
+                <div>
+                    <ActionButton setSelectedPage={setSelectedPage}>
+                        Join Now
+                    </ActionButton>
+                    <AnchorLink className='text-sm font-bold text-primary-500 underline hover:text-secondary-500'
+                     onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+                     href={`#${SelectedPage.ContactUs}`}>
+                        <p>Learn More</p>
+                    </AnchorLink>
+                </div>
+            </div>
+
+            {/* IMAGE */}
+            <div></div>
+        </div>
+    </section>
+  )
+}
+
+export default Home;
