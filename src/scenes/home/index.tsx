2c3a0ceeccd595376a3ca4688997cc237618c085
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-06-12 13:35:27 +0700

    Adding Motion ViewPortEnter

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -18,7 +18,9 @@ const Home = ({ setSelectedPage }: Props) => {
   return (
     <section id='home' className='gap-16 bg-gray-20 py-10 md:h-full md:pb-0'>
         {/* IMAGE AND MAIN HEADER */}
-        <div className='md:flex mx-auto w-5/6 items-center justify-center md:h-5/6'>
+        <motion.div className='md:flex mx-auto w-5/6 items-center justify-center md:h-5/6'
+                    onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+        >
             {/* MAIN HEADER */}
             <div  className='z-10 mt-32 md:basis-3/5'>
                 {/* HEADING */}
@@ -65,7 +67,7 @@ const Home = ({ setSelectedPage }: Props) => {
                  md:ml-40 md:mt-16 md:justify-items-end'>
                 <img alt='home-pageGraphic' src={HomePageGraphic} />
             </div>
-        </div>
+        </motion.div>
         {/* SPONSORS */}
         {isAboveMediumScreens && (
         <div className='h-[150px] w-full bg-primary-100 py-10'>
