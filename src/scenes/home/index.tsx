8dce1c8db31710919e7fb0a2355490e1ada652fb
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-06-12 13:31:59 +0700

    Handling Main Header with Framer Motion

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -7,6 +7,7 @@ import SponsorRedBull from '@/assets/SponsorRedBull.png';
 import SponsorForbes from '@/assets/SponsorForbes.png';
 import SponsorFoutune from '@/assets/SponsorFortune.png';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { motion } from 'framer-motion';
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
@@ -21,7 +22,14 @@ const Home = ({ setSelectedPage }: Props) => {
             {/* MAIN HEADER */}
             <div  className='z-10 mt-32 md:basis-3/5'>
                 {/* HEADING */}
-                <div className='md:-mt-20'>
+                <motion.div className='md:-mt-20'
+                        initial='hidden' whileInView='visible'
+                        viewport={{ once: true, amount: 0.5 }} transition={{ duration: 1 }}
+                        variants={{
+                            hidden: { opacity: 0, x: -50 },
+                            visible: { opacity: 1, x: 0 },
+                        }}
+                >
                     <div className='relative'>
                         <div className='before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext'>
                             <img alt='home-page-text' src={HomePageText} />
@@ -31,9 +39,16 @@ const Home = ({ setSelectedPage }: Props) => {
                         Unrivaled Gym. Unparalled Training Fitness Classes. World Class Studios to get
                         the Body Shapes That you Dream of...Get Your Dream Body Now.
                     </p>
-                </div>
+                </motion.div>
                 {/* ACTIONS */}
-                <div className='mt-8 flex items-center gap-8'>
+                <motion.div className='mt-8 flex items-center gap-8'
+                    initial='hidden' whileInView='visible'
+                    viewport={{ once: true, amount: 0.5 }} transition={{ delay: 0.2, duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, x: -50 },
+                        visible: { opacity: 1, x: 0 },
+                    }}
+                >
                     <ActionButton setSelectedPage={setSelectedPage}>
                         Join Now
                     </ActionButton>
@@ -42,7 +57,7 @@ const Home = ({ setSelectedPage }: Props) => {
                      href={`#${SelectedPage.ContactUs}`}>
                         <p>Learn More</p>
                     </AnchorLink>
-                </div>
+                </motion.div>
             </div>
 
             {/* IMAGE */}
