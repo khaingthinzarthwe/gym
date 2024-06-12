dc9146da3406f418d6e783cd90d679a1ed4d51ca
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-06-12 12:19:57 +0700

    Adding and Styling Content EvolveText

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -17,13 +17,13 @@ const Home = ({ setSelectedPage }: Props) => {
   return (
     <section id='home' className='gap-16 bg-gray-20 py-10 md:h-full md:pb-0'>
         {/* IMAGE AND MAIN HEADER */}
-        <div>
+        <div className='md:flex mx-auto w-5/6 items-center justify-center md:h-5/6'>
             {/* MAIN HEADER */}
-            <div>
+            <div  className='z-10 mt-32 md:basis-3/5'>
                 {/* HEADING */}
-                <div>
-                    <div>
-                        <div>
+                <div className='md:-mt-20'>
+                    <div className='relative'>
+                        <div className='before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext'>
                             <img alt='home-page-text' src={HomePageText} />
                         </div>
                     </div>
