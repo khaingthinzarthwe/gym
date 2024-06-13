067f5817d227f3ba0b9a628be34048ce56462945
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-06-13 16:48:11 +0700

    Styling Benefit Boxes

--- src/scenes/benefits/Benefit.tsx
+++ src/scenes/benefits/Benefit.tsx
@@ -1,5 +1,5 @@
 import { SelectedPage } from '@/shared/types';
-import React from 'react'
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 type Props = {
     icon: JSX.Element;
@@ -10,10 +10,20 @@ type Props = {
 
 const Benefit = ({ icon, title, description, setSelectedPage }: Props) => {
   return (
-    <div className=''>
-        <div className=''>
-            <div className=''></div>
+    <div className='mt-5 rounded-md border-2 border-gray-100 px-5 py-16 text-center'>
+        <div className='mb-4 flex justify-center'>
+            <div className='rounded-full border-2 border-gray-100 bg-primary-100 p-4'>
+                {icon}
+            </div>
         </div>
+        <h4 className='font-bold'>{title}</h4>
+        <p className='my-3'>{description}</p>
+        <AnchorLink className='text-sm font-bold text-primary-500 underline hover:text-secondary-500'
+            onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+            href={`#${SelectedPage.ContactUs}`}
+        >
+            <p>Learn More</p>
+        </AnchorLink>
     </div>
   )
 }
