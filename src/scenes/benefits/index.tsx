cfcb049e1d043ff0a8b453ae410a5f0a0d1e5830
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-06-13 17:48:08 +0700

    Added Animation to Benefits Header

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -37,14 +37,23 @@ const Benefits = ({ setSelectedPage }: Props) => {
   return (
     <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
         <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}>
+
             {/* HEADER */}
-            <div className="md:my-5 md:w-3/5">
+            <motion.div className="md:my-5 md:w-3/5"
+                initial='hidden' whileInView='visible'
+                viewport={{ once: true, amount: 0.5 }} transition={{ duration: 1 }}
+                variants={{
+                    hidden: { opacity: 0, x: -50 },
+                    visible: { opacity: 1, x: 0 },
+                }}
+            >
                 <HText>MORE THAN JUST A GYM.</HText>
                 <p className="my-5 text-sm">
                     We provide world class fitness equiment, trainers and classes to get you to your
                     ultimate fitness goals with ease. We provide true care into each and every member.
                 </p>
-            </div>
+            </motion.div>
+
             {/* BENEFITS */}
             <motion.div className="md:flex items-center justify-between gap-8 mt-5"
                  initial='hidden' whileInView='visible' viewport={{ once: true, amount: 0.5 }}
