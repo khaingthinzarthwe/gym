63ebbfc143885c43a006a4b03651f59d94fbf99b
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-06-14 11:57:36 +0700

    Building & Styling Benefits Page Graphics & Description

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -3,6 +3,8 @@ import { BenefitType, SelectedPage } from "@/shared/types";
 import { HomeModernIcon, UserGroupIcon, AcademicCapIcon } from "@heroicons/react/24/solid";
 import { motion } from "framer-motion";
 import Benefit from "./Benefit";
+import ActionButton from "@/shared/ActionButton";
+import BenefitsPageGraphic from '@/assets/BenefitsPageGraphic.png';
 
 const benefits: Array<BenefitType> = [
     {
@@ -68,6 +70,50 @@ const Benefits = ({ setSelectedPage }: Props) => {
                     />
                 ))}
             </motion.div>
+
+            {/* GRAPHICS AND DESCRIPTION */}
+            <div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex">
+                {/* GRAPHICS */}
+                <img className="mx-auto" alt="benefits-page-graphic" src={BenefitsPageGraphic} />
+
+                {/* DESCRIPTION */}
+                <div>
+                    {/* TITLE */}
+                    <div className="relative">
+                        <div className="before:absolute before:-top-20 before:-left-20 before:z-[1] before:content-abstractwaves">
+                            <div>
+                                <HText>
+                                    MILLIONS OF HAPPY MEMBERS GETTING{' '}
+                                    <span className="text-primary-500">FIT</span>
+                                </HText>
+                            </div>
+                        </div>
+                    </div>
+                    {/* DESCRIPT */}
+                    <div>
+                        <p className="my-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem unde dolor neque vel 
+                            voluptas sapiente quisquam, ipsa minima quia consectetur ab inventore ad ipsam placeat maiores
+                            laboriosam delectus optio quae!Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Consequatur blanditiis quod voluptates voluptas qui aliquid eos,aperiam vitae autem assumenda iure. 
+                            Aperiam natus inventore labore incidunt vel sunt tenetur eaque!
+                        </p>
+                        <p className="mb-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem, assumenda! Architecto sint,
+                            voluptate deserunt vitae deleniti quidem doloremque commodi! Suscipit quibusdam unde consequuntur
+                            nemo magni, nesciunt eos sapiente eius dolor. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Nemo assumenda veritatis consectetur voluptatibus ipsum corrupti dignissimos animi ut quos tenetur
+                            accusantium, minima, dolorem in eveniet maxime distinctio autem blanditiis numquam?
+                        </p>
+                    </div>
+                    {/* BUTTON */}
+                    <div className="relative mt-16">
+                        <div className="before:absolute before:-bottom-20 before:right-40 before:z-[1] before:content-sparkles">
+                            <ActionButton setSelectedPage={setSelectedPage}>
+                                Join Now
+                            </ActionButton>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </motion.div>
     </section>
   )
