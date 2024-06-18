36791f616c3f8208bf256842188439036a2ecd95
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-06-18 15:45:09 +0700

    Building & Styling ContactUs Page Form

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -9,6 +9,22 @@ type Props = {
 }
 
 const ContactUs = ({ setSelectedPage }: Props) => {
+    const inputStyles = `mt-5 w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white`
+
+    const {
+        register,
+        trigger,
+        formState: { errors }
+    } = useForm();
+    
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const onSubmit = async (e: any) => {
+        const isValid = await trigger();
+        if(!isValid) {
+            e.preventDefault();
+        }
+    }
+
   return (
     <section id='contactus' className='mx-auto w-5/6 pt-24 pb-32'>
         <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}>
@@ -29,6 +45,70 @@ const ContactUs = ({ setSelectedPage }: Props) => {
                 Officia cum et ipsam corporis repellendus aliquam? Quidem, soluta placeat magnam qui
                 aliquid fugit repellat non est.</p>
             </motion.div>
+            {/* FORM AND IMAGE */}
+            <div className='mt-10 justify-between gap-8 md:flex'>
+                <motion.div className='mt-10 basis-3/5 md:mt-0'
+                    initial='hidden' whileInView='visible'
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, y: 50 },
+                        visible: { opacity: 1, y: 0 },
+                    }}
+                >
+                    <form target='_blank' onSubmit={onSubmit} action='http://formsubmit.co/khaingthinzarthwe3111@gmail.com' method='POST'>
+                        <input className={inputStyles}
+                            type='text'
+                            placeholder='NAME'
+                            {...register('name', {
+                                required: true,
+                                maxLength: 100,
+                            })}
+                        />
+                        {errors.name && (
+                            <p className='mt-1 text-primary-500'>
+                                {errors.name.type === 'required' && 'This field is required.'}
+                                {errors.name.type === 'maxLength' && 'Max Length is 100 char.'}
+                            </p>
+                        )}
+
+                        <input className={inputStyles}
+                            type='text'
+                            placeholder='EMAIL'
+                            {...register('email', {
+                                required: true,
+                                pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                            })}
+                        />
+                        {errors.email && (
+                            <p className='mt-1 text-primary-500'>
+                                {errors.email.type === 'required' && 'This field is required.'}
+                                {errors.email.type === 'pattern' && 'Invalid Email Address'}
+                            </p>
+                        )}
+
+                        <textarea className={inputStyles}
+                            rows={4} cols={50}
+                            placeholder='MESSAGE'
+                            {...register('message', {
+                                required: true,
+                                maxLength: 2000,
+                            })}
+                        />
+                        {errors.message && (
+                            <p className='mt-1 text-primary-500'>
+                                {errors.message.type === 'required' && 'This field is required.'}
+                                {errors.message.type === 'maxLength' && 'Max Length is 2000 char.'}
+                            </p>
+                        )}
+
+                        <button type='submit'
+                            className='mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white'>
+                                SUBMIT
+                        </button>
+                    </form>
+                </motion.div>
+            </div>
         </motion.div>
     </section>
   )
